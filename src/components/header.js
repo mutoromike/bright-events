a3bda6b3f2beb649338c27e20dde93909c636e72
Michael <strongestavenger@Michaels-MacBook-Pro.local>
2018-04-24 14:20:57 +0300

    [feat] add component to handle navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,48 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { headers } from '../constants/common';
 
 class Header extends Component {
 
+  state = {
+    message: "",
+    redirect: false
+    }
+
+  logoutUser = (event) => {
+    event.preventDefault(); // prevent form auto-reloads
+    axios({
+        method:'post',
+        url:'http://localhost:8000/api/v2/auth/logout',
+        headers: headers
+    }).then((resp) => {
+        console.log(resp);
+        toast.success(resp.data.message)
+        localStorage.removeItem('Token')
+        localStorage.clear();
+        console.log("you have been logged out")
+        this.setState({redirect: true});
+        // if (resp.data.access_token){
+        //     console.log("You are logged in dumbass!!");
+        //     this.setState({redirect: true});
+        // }
+        // else{
+        //     this.setState({redirect: true});
+        //     console.log("Login you motherfucker!");
+        // }
+    }).catch((err) => {
+        // err.response.data\
+        toast.error(err.response.data.message)
+    })
+    }
+
   render() {
+    if(this.state.redirect){
+      return (<Redirect to={'/login'}/>)
+    }
     return (
       <div className="header clearfix">
         <nav className="navbar navbar-inverse navbar-fixed-top">
@@ -34,13 +73,25 @@ class Header extends Component {
 					  </button>
         </div>
                 <div className="collapse navbar-collapse" id="main-navigation">
-                    <ul className="nav navbar-nav navbar-center">
+                    <ul className="nav navbar-nav navbar-center dropdown ">
                         <li>
-                            <Link to="/" className="page-scroll">Home</Link>
+                            <Link to="/home" className="page-scroll">Home</Link>
                         </li>
                         <li>
                             <Link to="/events" className="page-scroll">Events</Link>
                         </li>
+                        <li>
+                            <Link to="/myEvents" className="page-scroll">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="" className="page-scroll dropdown-toggle" data-toggle="dropdown"><i className="glyphicon glyphicon-user"></i>
+                            </Link>
+                        <ul className="dropdown-menu">
+                          <li><a href="#">Dashboard</a></li>
+                          <li><a href="#">Profile</a></li>
+                          <li onClick={this.logoutUser}>Logout</li>
+                        </ul>
+                        </li> 
                         <li>
                             <Link to="/register" className="page-scroll">Register</Link>
                         </li>
@@ -55,5 +106,6 @@ class Header extends Component {
     );
   }
 }
+  
 
 export default Header;
