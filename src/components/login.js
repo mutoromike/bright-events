b3e5fea4d605dc98f504e04901a4a03df2d09f96
Michael <strongestavenger@Michaels-MacBook-Pro.local>
2018-04-24 14:21:25 +0300

    [feat] add component to handle login

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,68 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { headers } from '../constants/common';
+
 
 class Login extends Component {
+    state = {
+      message: '',
+      form: {
+        email: '',
+        password: '',
+        redirect: false
+      }
+    }
+    loginSubmit = (event) => {
+      event.preventDefault(); // prevent form auto-reloads
+      // console.log('Event: Form Submit', this.usernameInput.value);
+      // axios.get('https://bright-events-api.herokuapp.com/api/v2/auth/register')
+      axios({
+        method: 'post',
+        url: 'http://localhost:8000/api/v2/auth/login',
+        headers,
+        data: this.state.form
+      }).then((resp) => {
+        toast.success(resp.data.message);
+        localStorage.setItem('Token', resp.data.access_token);
+        if (resp.data.access_token) {
+          console.log('You are logged in!!');
+          this.setState({ redirect: true });
+        } else {
+          this.setState({ redirect: false });
+          console.log('Login you idiot!');
+        }
+      }).catch((err) => {
+        // err.response.data\
+        toast.error(err.response.data.message);
+      });
+    }
 
-  render() {
-    return (
+    onChange = (event) => {
+      const myState = this.state;
+      myState.form[event.target.name] = event.target.value;
+      this.setState(myState);
+    }
+    render() {
+      const { form } = this.state;
+
+      if (this.state.redirect) {
+        return (<Redirect to={'/myEvents'}/>);
+      }
+      return (
         <div className="container page-content">
         <br />
         <br />
         <br />
         <br />
+        <ToastContainer
+        hideProgressBar={true}
+        newestOnTop={true}
+        autoClose={3000}
+        />
             <div id="loginbox" className="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        
+
                 <div className="panel panel-login ">
                     <div className="panel-heading">
                         <div className="row">
@@ -21,20 +73,21 @@ class Login extends Component {
                         <hr />
                     </div>
                     {/* panel-heading */}
-                    <div style={{paddingTop: 30}} className="panel-body">
-        
-                        <form id="loginform" action="/api/v1/auth/login" method="post" className="form-horizontal" role="form">
-        
-                            <div style={{marginBottom: 25}} className="input-group ">
-                                <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i>
+                    <div style={{ paddingTop: 30 }} className="panel-body">
+                        <form id="loginform" onSubmit = { this.loginSubmit } method="post" className="form-horizontal">
+
+                            <div style={{ marginBottom: 25 }} className="input-group ">
+                                <span className="input-group-addon"><i className="glyphicon glyphicon-envelope"></i>
                                 </span>
-                                <input type="username" className="form-control" ref="username" placeholder="Username" required />
+                                <input type="text" name='email' className="form-control"
+                                onChange = {this.onChange} placeholder="Email" required />
                             </div>
-        
-                            <div style={{marginBottom: 25}} className="input-group">
+
+                            <div style={{ marginBottom: 25 }} className="input-group">
                                 <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i>
                                 </span>
-                                <input type="password" className="form-control" ref="password" placeholder="Password " required />
+                                <input type="password" name='password' className="form-control"
+                                onChange = {this.onChange} placeholder="Password " required />
                             </div>
                             <div className="form-group">
                                 <div className="row">
@@ -44,7 +97,7 @@ class Login extends Component {
                                 </div>
                             </div>
                             <div className="form-group text-center">
-                                <a href="/api/v1/auth/register" className="forgot-password">Don't have an account?Register</a>
+                                <Link to="/register" className="forgot-password">Don't have an account?Register</Link>
                             </div>
                         </form>
                     </div>
@@ -55,8 +108,8 @@ class Login extends Component {
             {/* loginbox */}
         </div>
         // {/* container */}
-    );
-  }
+      );
+    }
 }
 
 export default Login;
