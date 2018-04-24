f45e03de65d995917c0f7153ed48f50778d1b299
Michael <strongestavenger@Michaels-MacBook-Pro.local>
2018-04-24 14:18:55 +0300

    [feat] add home component

---
+++ src/components/home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import '../App.css';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <header>
+                
+                <div className="header-content">
+                    <div className="header-content-inner">
+                        <h1 className="heading"> Bright Events </h1>
+                        <hr />
+                        <p>Platform to create and manage different types of events. 
+                        
+                        <br />
+                        
+                            <Link to="/login">RSVP </Link>to events. 
+                        </p>		
+                        <p>Organizing for events? <Link to="register"> Register </Link> today. 
+                        </p>
+                        
+                    </div>
+
+                </div>
+            </header>
+
+        );
+    }
+}
+
+export default Home;
